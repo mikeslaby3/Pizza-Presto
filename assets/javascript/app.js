009cdda03f2a47208471277c741b0aa13de9038c
Mike Slaby <mikeslaby3@gmail.com>
2019-10-10 15:10:54 -0700

    Cleaned up code by getting rid of comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,13 @@
-//========================================================//
-//Global Variables
-//========================================================//
 var username = "";
 var userScore = 0;
 var difficulty = "";
-var maxSeconds = 30;
+var maxSeconds = 120;
 var secondsRemaining;
 var highScore = "";
 var intervalTimer;
 var timeWhenLastPizzaWasComplete = 120;
 var database;
 
-//Music
 var introMusic = document.getElementById("introMusic");
 var gameMusic = document.getElementById("gameMusic");
 var scoreboardMusic = document.getElementById("scoreboardMusic");
@@ -29,9 +25,6 @@ var answerDisplay2 = $("#answer2");
 var answerDisplay3 = $("#answer3");
 var answerDisplay4 = $("#answer4");
 
-//====================================================//
-// Chef Variables
-//====================================================//
 var chefDisapprovalDialogueOptions = [
   "Mamma mia...",
   "My goldfish can make better pizza",
@@ -49,9 +42,6 @@ var chefGoodDialogue = [
   "Ah, a fresh pizza makes me think of home"
 ];
 
-//====================================================//
-// Pizza Variables
-//====================================================//
 var currentPizzaOrder;
 var possiblePizzas = [
   "pepperoni",
@@ -62,14 +52,8 @@ var possiblePizzas = [
 ];
 var ingredientCount = 0;
 
-//========================================================//
-// Main Run Through
-//========================================================//
 $(document).ready(function () {
   initializaFirebaseAndCheckForHighScores();
-  // //============================//
-  // // Initialize Firebase
-  // //============================//
   // var config = {
   //   apiKey: "AIzaSyDd-uc53MHvSpaahIvBuYI2oAG22eZLkuw",
   //   authDomain: "pizza-presto-28c03.firebaseapp.com",
@@ -91,7 +75,7 @@ $(document).ready(function () {
       secondsRemaining = maxSeconds;
       intervalTimer = setInterval(timerTick, 1000);
       triviaPull();
-      // prevent page from refreshing when form tries to submit itself
+
       event.preventDefault();
       $("#settingsMenu").addClass("hide");
       $("#gameScreen").removeClass("hide");
@@ -104,7 +88,6 @@ $(document).ready(function () {
       //   username: name
       // });
 
-      //Makes the username for the Game Screen into the User's settings
       $("#username-display").text(name);
 
       // database.ref("/users").on("value", function (snapshot) {
@@ -122,17 +105,12 @@ $(document).ready(function () {
     });
   }
 
-  //============================//
-  //Materialize Animations
-  //============================//
   $(".fixed-action-btn").floatingActionButton();
-  $("select").formSelect(); //For the select difficulty dropdown
+  $("select").formSelect();
   $(document).ready(function () {
     $(".tooltipped").tooltip();
   });
-  //============================//
-  // Add Event Listeners
-  //============================//
+
   playButtonClicked();
   startGameButtonClicked();
   addScoreboardButtonListeners();
@@ -140,18 +118,9 @@ $(document).ready(function () {
   addMuteUnmuteButtonListeners();
   addListenerToTheBottomRightFloatingRestartButton();
 
-  // //============================//
-  // // Display Pizza
-  // //============================//
   // generateRandomPizzaOrder();
   // displayPizzaOrder();
 });
-
-//========================================================//
-// APIs
-//========================================================//
-//On the scoreboard page, the chef will say a random pizza joke
-//on the bottom
 
 function triviaPull() {
   difficulty = $("#difficulty")
@@ -172,7 +141,6 @@ function triviaPull() {
 }
 
 function renderQuestion() {
-  // Finds the HTML symbols in the questions/answers and replaces them with readable symbols
   function replaceWeirdSymbols(question) {
     return question
       .replace(/&quot;/g, '"')
@@ -189,13 +157,10 @@ function renderQuestion() {
       .replace(/&rsquo;/g, '"');
   }
 
-  // Grabs the first question out the API data and stores it in current question variable
   currentQuestion = questionArray[questionIndex];
 
-  // Variable storing the trivia question
   var triviaQuestion = replaceWeirdSymbols(currentQuestion.question);
 
-  // Variables storing the correct answer and three incorrect answers
   correctAnswer = replaceWeirdSymbols(currentQuestion.correct_answer);
   var incorrectAnswer1 = replaceWeirdSymbols(
     currentQuestion.incorrect_answers[0]
@@ -207,7 +172,6 @@ function renderQuestion() {
     currentQuestion.incorrect_answers[2]
   );
 
-  // Array with multiple choice answers
   answerArray = [
     incorrectAnswer1,
     incorrectAnswer2,
@@ -215,17 +179,8 @@ function renderQuestion() {
     correctAnswer
   ];
 
-  // This array is used to mix up the answers so they
-  // don't appear on the same buttons every time.
-  // We repeat -1 so that we can easily recognize
-  // when a random number has already been used.
   randomNumberArray = [-1, -1, -1, -1];
 
-  // This function is used to check if the random number
-  // has already been put in the array by looping through
-  // the random number array and if the number isn't already
-  // in the array, it returns false and continues looping,
-  // but if it already exists in the array, it returns true
   function randomNumberIsInArray(randomNumber) {
     var isInArray = false;
     for (let index = 0; index < 4; index++) {
@@ -236,11 +191,6 @@ function renderQuestion() {
     return isInArray;
   }
 
-  // This function says that when we create a new random number,
-  // if the random number is not already in the array, it is
-  // valid so add it to the array and continue the loop. However,
-  // if the random number is already in the array, stop the loop
-  // and generate another random number
   function getValidRandomNumber() {
     var randomNumber;
     invalidNumber = true;
@@ -253,18 +203,14 @@ function renderQuestion() {
     return randomNumber;
   }
 
-  // This funcation takes a valid random number that hasn't
-  // already been used and adds it to the random number array
   function randomizeIndexNumber() {
     for (let index = 0; index < 4; index++) {
       randomNumberArray[index] = getValidRandomNumber();
     }
   }
 
-  // Call the function above to create random number array
   randomizeIndexNumber();
 
-  // Display the random answers by plugging each random number into the answer array
   questionDisplay.text(triviaQuestion);
   answerDisplay1.text(answerArray[randomNumberArray[0]]);
   answerDisplay2.text(answerArray[randomNumberArray[1]]);
@@ -311,7 +257,6 @@ function finishedPizzaBounceOutAnimation() {
   }, 2000);
 }
 
-// Finds the HTML symbols in the questions/answers and replaces them with readable symbols
 function replaceWeirdSymbols(question) {
   return question
     .replace(/&quot;/g, '"')
@@ -322,9 +267,6 @@ function replaceWeirdSymbols(question) {
     .replace(/&amp;/g, "&");
 }
 
-//=======================================================================================//
-//Joke API
-//=======================================================================================//
 function joke() {
   var queryURL = "https://official-joke-api.appspot.com/random_joke";
 
@@ -345,9 +287,6 @@ function joke() {
   });
 }
 
-//========================================================================================================//
-// Chef Animations
-//========================================================================================================//
 function chefDisapproval() {
   $("#dialogue").text(
     chefDisapprovalDialogueOptions[
@@ -373,19 +312,10 @@ function chefApproval() {
   $("#chef").attr("src", "./assets/images/happyChefwPizza.png");
 }
 
-//========================================================================================================//
-// Pizza Functions
-//========================================================================================================//
-//====================================================//
-// Choose a random pizza
-//====================================================//
 function generateRandomPizzaOrder() {
   currentPizzaOrder = possiblePizzas[Math.floor(Math.random() * 5)];
 }
 
-//====================================================//
-// Display the Pizza Order
-//====================================================//
 function displayPizzaOrder() {
   let index = possiblePizzas.indexOf(currentPizzaOrder);
   if (index === 0) {
@@ -416,11 +346,6 @@ function displayPizzaOrder() {
   }
 }
 
-//=======================================================================================//
-// Screen Changes
-//=======================================================================================//
-
-//Adds an event listener to the play button, which brings us to the next screen
 function playButtonClicked() {
   $(document).on("click", "#playButton", function () {
     $("#menuScreen").addClass("hide");
@@ -439,10 +364,6 @@ function gameOver() {
   joke();
 }
 
-// where function used to be
-
-//Shows the scoreBoard, gives the user the option to replay the game,
-//or choose a new topic
 function addScoreboardButtonListeners() {
   replayChangeToSettingScreen();
 }
@@ -467,7 +388,6 @@ function addListenerToTheBottomRightFloatingRestartButton() {
     location.reload();
   });
 }
-
 
 function addAnswerButtonListeners() {
   $(document).on("click", ".answer-button", function () {
@@ -532,9 +452,6 @@ function replayChangeToSettingScreen() {
   });
 }
 
-//=======================================================================================//
-// Timer
-//=======================================================================================//
 function timeConverter(timeInSeconds) {
   var minutes = Math.floor(timeInSeconds / 60);
   var seconds = timeInSeconds - minutes * 60;
@@ -586,9 +503,7 @@ function timer() {
     clearInterval(timerTickID);
   }, 3 * 1000);
 }
-//=======================================================================================//
-// Music
-//=======================================================================================//
+
 function startIntroMusic() {
   if (currentSong == "game") {
     gameMusic.pause();
@@ -625,27 +540,15 @@ function startScoreboardMusic() {
   scoreboardMusic.play();
 }
 
-//=======================================================================================//
-// Firebase
-//=======================================================================================//
-//Updates High Scores to firebase
 function checkScores() {
-  // check if there's a high score in the database
   var highScore;
   var currentScore = $('#score').val();
   var currentUser = $('#username').val();
 
   database.ref('/scores').on('value', function () {
-    // go through the array of scores, look for one with a username
-    // {
-    //   username: "bob",
-    //   highScore: 14
-    // }
-    // if we find one, set highScore to compare later
     if (currentUser === snapshot.val()[0].username) {
       highScore = snapshot.val()[0].highScore;
     } else {
-      // set the current value to the high score
       database.ref('/score').set({
         username: currentUser,
         highScore: currentScore
@@ -654,34 +557,17 @@ function checkScores() {
     }
   })
 
-  // if there is, check to see if we need to update it
   if (highScore) {
     if (currentScore > highScore) {
-      // update the database with the new high score
       database.ref('/score').set({
         userName: currentUser,
         highScore: currentScore
       })
     }
   }
-  // if not, push a new object to the database
 }
 
-// // after the time expires, call checkScores
-// setTimeout(checkScores, time);
-
-// pseudocode
-
-// function def
-// set up variables
-// access db and look for username
-// if there is a username, get the high score
-// if there isn't, then set the current score as the high score
-// if the username was found, check to see if the current score is larger than the high score
-// if the current score is greater than the high score, update the db with the new high score
-
 function initializaFirebaseAndCheckForHighScores() {
-  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCwUmc_twZWmStRkiWZ8zuSjc2dH_ccY1s",
     authDomain: "coderbay-c1c1a.firebaseapp.com",
@@ -692,53 +578,33 @@ function initializaFirebaseAndCheckForHighScores() {
   };
   firebase.initializeApp(config);
 
-  // Create a variable to reference the database
   database = firebase.database();
 
-  // Initial Values
   var highScore = 0;
   var highScoreUser = "No one";
 
-  // --------------------------------------------------------------
-
-  // At the initial load and subsequent value changes, get a snapshot of the stored data.
-  // This function allows you to update your page in real-time when the firebase database changes.
   database.ref().on("value", function (snapshot) {
-
-    // If Firebase has a highScore and highScoreUser stored, update our client-side variables
     if (snapshot.child("highScoreUser").exists() && snapshot.child("highScore").exists()) {
-      // Set the variables for highScoreUser/highScore equal to the stored values.
       highScoreUser = snapshot.val().highScoreUser;
       highScore = parseInt(snapshot.val().highScore);
     }
-
-    // If Firebase does not have highScore and highScoreUser values stored, they remain the same as the
-    // values we set when we initialized the variables.
-    // In either case, we want to log the values to console and display them on the page.
     console.log(highScoreUser);
     console.log(highScore);
     $("#name1").text(highScoreUser);
     $('#highScore').text(highScore)
     $("#score1").text(highScore);
 
-    // If any errors are experienced, log them to console.
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
-
-
 }
 
 function checkIfUserBeatTheHighScore() {
   if (userScore > highScore) {
-    // Save the new price in Firebase. This will cause our "value" callback above to fire and update
-    // the UI.
     database.ref().set({
       highScoreUser: username,
       highScore: userScore
     });
-
-    // Log the new High Price
     console.log("New High Score!");
     console.log(username);
     console.log(userScore);
